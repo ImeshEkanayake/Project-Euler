c12e4e30b768dd905c5efa195c7d4e6004c7daef
Imesh Ekanayake <imeshuek059@gmail.com>
2018-03-09 16:33:02 +0530

    Update C001.c

--- 001/C001.c
+++ 001/C001.c
@@ -8,10 +8,12 @@ int main(){
 	printf("Enter the range : ");
 	int P,p3,p5,p15,sum;
 	scanf("%d",&P);
-	P=P-1;
-	p3=P/3;
-	p5=P/5;
-	p15=P/15;
+	P=P-1;  //until the input
+	
+	p3=P/3; //maximum multiple of 3 below the input
+	p5=P/5; //maximum multiple of 5 below the input
+	p15=P/15;//maximum multiple of 15 below the input
+	
 	sum=sum_of_range(p3)*3 + sum_of_range(p5)*5 - sum_of_range(p15)*15;
 	printf("The tota sum is %d\n",sum );
 
